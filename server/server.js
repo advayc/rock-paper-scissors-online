a0c730b7208afb29a4393cdc6446dd54687028ad
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-06-26 01:50:07 -0400

    tried to fix issues regarding CORS

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 const PORT = process.env.PORT || 4000;
+
+// Enable CORS for all origins
+app.use(cors());
 
 let players = {};
 let choices = {};
